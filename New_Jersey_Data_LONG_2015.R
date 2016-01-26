a52c21d210ee2f140cc58ff85ed649044bbd0a48
Adam VanIwaarden <avaniwaarden@gmail.com>
2016-01-25 17:43:02 -0700

    Final edits for 2015 analyses

--- New_Jersey_Data_LONG_2015.R
+++ New_Jersey_Data_LONG_2015.R
@@ -12,8 +12,8 @@ require(data.table)
 
 ### Load 2015 raw PARCC data
 
-NJ_ELA <- fread("Data/Base_Files/NJ_PARCC_2014_2015_ELA.csv", sep="|", colClasses=rep("character", 37)) # Stopped reading at empty line 773712 but text exists afterwards (discarded): (773710 row(s) affected)
-NJ_MATH<- fread("Data/Base_Files/NJ_PARCC_2014_2015_MATHEMATICS.csv", sep="|", colClasses=rep("character", 37)) # ... (discarded): (745646 row(s) affected)
+NJ_ELA <- fread("Data/Base_Files/PARCC Data Extract for NCIEA 14-15 SY_ELA_count=773710_repull.csv", sep="|", colClasses=rep("character", 37)) # Stopped reading at empty line 773712 but text exists afterwards (discarded): (773710 row(s) affected)
+NJ_MATH<- fread("Data/Base_Files/PARCC Data Extract for NCIEA 14-15 SY_Math related_count=745646_repull.csv", sep="|", colClasses=rep("character", 37)) # ... (discarded): (745646 row(s) affected)
 
 New_Jersey_Data_LONG_2015 <- rbindlist(list(NJ_ELA, NJ_MATH))
 
@@ -25,12 +25,12 @@ New_Jersey_Data_LONG_2015 <- rbindlist(list(NJ_ELA, NJ_MATH))
 setnames(New_Jersey_Data_LONG_2015, names(New_Jersey_Data_LONG_2015), gsub(" |/", "", names(New_Jersey_Data_LONG_2015)))	
 
 setnames(New_Jersey_Data_LONG_2015, 
-				 c("esID", "SSID", "IRTTheta", "SummativeScaleScore", "SummativeCSEM", "Subject", "GradeLevelWhenAssessed", "SummativePerformanceLevel",
+				 c("SSID", "IRTTheta", "SummativeScaleScore", "SummativeCSEM", "Subject", "GradeLevelWhenAssessed", "SummativePerformanceLevel",
 				 	"ResponsibleSchoolInstitutionIdentifier", "ResponsibleSchoolInstitutionName", "ResponsibleDistrictIdentifier", "ResponsibleDistrictName", "ResponsibleCountyName",
 				 	"SpecialEducationClassification", "TitleIIILimitedEnglishProficientParticipationStatus", "EconomicallyDisadvantaged"),
-				 c("ID", "Student.ID..SSID.", "SCALE_SCORE", "SCALE_SCORE_ACTUAL", "SCALE_SCORE_CSEM", "CONTENT_AREA", "GRADE", "ACHIEVEMENT_LEVEL",
-				 	"SCHOOL_NUMBER", "SCHOOL_NAME", "DISTRICT_NUMBER", "DISTRICT_NAME", "County.Name",
-				 	"Special.Education..SE.", "Current.LEP", "Economically.Disadvantaged"))
+				 c("ID", "SCALE_SCORE", "SCALE_SCORE_ACTUAL", "SCALE_SCORE_CSEM", "CONTENT_AREA", "GRADE", "ACHIEVEMENT_LEVEL",
+				 	"SCHOOL_NUMBER", "SCHOOL_NAME", "DISTRICT_NUMBER", "DISTRICT_NAME", "County_Name",
+				 	"Special_Education__SE_", "Current_LEP", "Economically_Disadvantaged"))
 
 ####  YEAR
 New_Jersey_Data_LONG_2015[, YEAR := '2015']
@@ -48,15 +48,15 @@ New_Jersey_Data_LONG_2015[which(!CONTENT_AREA %in% c("ELA", "MATHEMATICS")), GRA
 New_Jersey_Data_LONG_2015[, ACHIEVEMENT_LEVEL := paste("Level", ACHIEVEMENT_LEVEL)]
 
 ####  Demographic Variables
-New_Jersey_Data_LONG_2015[, Race.Ethnicity.Combined := as.character(NA)] 
-New_Jersey_Data_LONG_2015[which(HispanicorLatinoEthnicity=="Y"), Race.Ethnicity.Combined := "Hispanic"] 
-New_Jersey_Data_LONG_2015[which(AmericanIndianorAlaskaNative=="Y"), Race.Ethnicity.Combined := "Native American"] 
-New_Jersey_Data_LONG_2015[which(Asian=="Y"), Race.Ethnicity.Combined := "Asian"] 
-New_Jersey_Data_LONG_2015[which(BlackorAfricanAmerican=="Y"), Race.Ethnicity.Combined := "Black"] 
-New_Jersey_Data_LONG_2015[which(NativeHawaiianorOtherPacificIslander=="Y"), Race.Ethnicity.Combined := "Pacific Islander"] 
-New_Jersey_Data_LONG_2015[which(White=="Y"), Race.Ethnicity.Combined := "White"] 
-New_Jersey_Data_LONG_2015[which(TwoorMoreRaces=="Y"), Race.Ethnicity.Combined := "Two or More Races"] 
-New_Jersey_Data_LONG_2015[which(is.na(Race.Ethnicity.Combined)), Race.Ethnicity.Combined := "Other"] 
+New_Jersey_Data_LONG_2015[, Race_Ethnicity_Combined := as.character(NA)] 
+New_Jersey_Data_LONG_2015[which(HispanicorLatinoEthnicity=="Y"), Race_Ethnicity_Combined := "Hispanic"] 
+New_Jersey_Data_LONG_2015[which(AmericanIndianorAlaskaNative=="Y"), Race_Ethnicity_Combined := "Native American"] 
+New_Jersey_Data_LONG_2015[which(Asian=="Y"), Race_Ethnicity_Combined := "Asian"] 
+New_Jersey_Data_LONG_2015[which(BlackorAfricanAmerican=="Y"), Race_Ethnicity_Combined := "Black"] 
+New_Jersey_Data_LONG_2015[which(NativeHawaiianorOtherPacificIslander=="Y"), Race_Ethnicity_Combined := "Pacific Islander"] 
+New_Jersey_Data_LONG_2015[which(White=="Y"), Race_Ethnicity_Combined := "White"] 
+New_Jersey_Data_LONG_2015[which(TwoorMoreRaces=="Y"), Race_Ethnicity_Combined := "Two or More Races"] 
+New_Jersey_Data_LONG_2015[which(is.na(Race_Ethnicity_Combined)), Race_Ethnicity_Combined := "Other"] 
 
 New_Jersey_Data_LONG_2015[, 
 		c("HispanicorLatinoEthnicity", "AmericanIndianorAlaskaNative", "Asian", "BlackorAfricanAmerican", "NativeHawaiianorOtherPacificIslander", "White", "TwoorMoreRaces") := NULL]
@@ -64,24 +64,24 @@ New_Jersey_Data_LONG_2015[,
 New_Jersey_Data_LONG_2015[,Gender:=as.factor(Gender)]
 setattr(New_Jersey_Data_LONG_2015$Gender, "levels", c("Female", "Male"))
 
-New_Jersey_Data_LONG_2015[Special.Education..SE. == "", Special.Education..SE. := as.character(NA)]
+New_Jersey_Data_LONG_2015[Special_Education__SE_ == "", Special_Education__SE_ := as.character(NA)]
 
-New_Jersey_Data_LONG_2015[Current.LEP == "", Current.LEP := as.character(NA)]
-New_Jersey_Data_LONG_2015[,Current.LEP:=factor(Current.LEP)]
-setattr(New_Jersey_Data_LONG_2015$Current.LEP, "levels", c("No", "Yes"))
-New_Jersey_Data_LONG_2015[,Current.LEP:=as.character(Current.LEP)]
+New_Jersey_Data_LONG_2015[Current_LEP == "", Current_LEP := as.character(NA)]
+New_Jersey_Data_LONG_2015[,Current_LEP:=factor(Current_LEP)]
+setattr(New_Jersey_Data_LONG_2015$Current_LEP, "levels", c("No", "Yes"))
+New_Jersey_Data_LONG_2015[,Current_LEP:=as.character(Current_LEP)]
 
-New_Jersey_Data_LONG_2015[Economically.Disadvantaged=="", Economically.Disadvantaged:=as.character(NA)]
-New_Jersey_Data_LONG_2015[,Economically.Disadvantaged:=factor(Economically.Disadvantaged)]
-setattr(New_Jersey_Data_LONG_2015$Economically.Disadvantaged, "levels", c("Economically Disadvantaged: No", "Economically Disadvantaged: Yes"))
-New_Jersey_Data_LONG_2015[,Economically.Disadvantaged:=as.character(Economically.Disadvantaged)]
+New_Jersey_Data_LONG_2015[Economically_Disadvantaged=="", Economically_Disadvantaged:=as.character(NA)]
+New_Jersey_Data_LONG_2015[,Economically_Disadvantaged:=factor(Economically_Disadvantaged)]
+setattr(New_Jersey_Data_LONG_2015$Economically_Disadvantaged, "levels", c("Economically Disadvantaged: No", "Economically Disadvantaged: Yes"))
+New_Jersey_Data_LONG_2015[,Economically_Disadvantaged:=as.character(Economically_Disadvantaged)]
 
 New_Jersey_Data_LONG_2015[Migrant == "", Migrant := as.character(NA)]
 New_Jersey_Data_LONG_2015[,Migrant:=factor(Migrant)]
 setattr(New_Jersey_Data_LONG_2015$Migrant, "levels", c("Migrant: No", "Migrant: Yes"))
 New_Jersey_Data_LONG_2015[,Migrant:=as.character(Migrant)]
 
-####  District and School ID and Names
+####  District and School Names
 New_Jersey_Data_LONG_2015[,DISTRICT_NAME:=as.factor(DISTRICT_NAME)]
 setattr(New_Jersey_Data_LONG_2015$DISTRICT_NAME, "levels", sapply(levels(New_Jersey_Data_LONG_2015$DISTRICT_NAME), capwords))
 
@@ -98,6 +98,7 @@ New_Jersey_Data_LONG_2015[,SCHOOL_ENROLLMENT_STATUS:=factor(1, levels=0:1, label
 ####  Set SCALE_SCORE to numeric and only include non-NA scores in long data
 New_Jersey_Data_LONG_2015[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
 New_Jersey_Data_LONG_2015[,SCALE_SCORE_ACTUAL:=as.numeric(SCALE_SCORE_ACTUAL)]
+New_Jersey_Data_LONG_2015[,SCALE_SCORE_CSEM:=as.numeric(SCALE_SCORE_CSEM)]
 New_Jersey_Data_LONG_2015 <- New_Jersey_Data_LONG_2015[!is.na(SCALE_SCORE)]
 
 ###
@@ -106,28 +107,83 @@ New_Jersey_Data_LONG_2015 <- New_Jersey_Data_LONG_2015[!is.na(SCALE_SCORE)]
 
 New_Jersey_Data_LONG_2015[, VALID_CASE := "VALID_CASE"]
 
-####  Invalidate Cases with missing IDs - Not and issue with esIDs
-# New_Jersey_Data_LONG_2015[which(ID==""), VALID_CASE := "INVALID_CASE"]
+###  Invalidate Cases with missing IDs - Not and issue with esIDs
+New_Jersey_Data_LONG_2015[which(ID==""), VALID_CASE := "INVALID_CASE"]
 
-setkey(New_Jersey_Data_LONG_2015, VALID_CASE, CONTENT_AREA, GRADE, ID)
-# sum(duplicated(New_Jersey_Data_LONG_2015[VALID_CASE != "INVALID_CASE"])) # 16 duplicates with valid SSIDs -- all have same SSID and esID, so appear valid - take the highest score
-# dups <- data.table(New_Jersey_Data_LONG_2015[unique(c(which(duplicated(New_Jersey_Data_LONG_2015))-1, which(duplicated(New_Jersey_Data_LONG_2015)))), ], key=key(New_Jersey_Data_LONG_2015))
+###  Invalidate Grades not used:
+New_Jersey_Data_LONG_2015[which(GRADE %in% c(NA,1,2,12)), VALID_CASE := "INVALID_CASE"] #  11th grade ELA not used, but leave it Valid just in case.
+New_Jersey_Data_LONG_2015[which(GRADE %in% 9:11 & CONTENT_AREA == "MATHEMATICS"), VALID_CASE := "INVALID_CASE"]
+
+###  Invalidate Content Areas with too few students to analyze
+New_Jersey_Data_LONG_2015[which(CONTENT_AREA %in% c("INTEGRATED_MATHEMATICS_I", "INTEGRATED_MATHEMATICS_II", "INTEGRATED_MATHEMATICS_III")), VALID_CASE := "INVALID_CASE"]
+
 setkey(New_Jersey_Data_LONG_2015, VALID_CASE, CONTENT_AREA, GRADE, ID, SCALE_SCORE)
 setkey(New_Jersey_Data_LONG_2015, VALID_CASE, CONTENT_AREA, GRADE, ID)
+# sum(duplicated(New_Jersey_Data_LONG_2015[VALID_CASE != "INVALID_CASE"])) # 10 duplicates with valid SSIDs -- all have same SSID and esID, so appear valid - take the highest score
+# dups <- data.table(New_Jersey_Data_LONG_2015[unique(c(which(duplicated(New_Jersey_Data_LONG_2015))-1, which(duplicated(New_Jersey_Data_LONG_2015)))), ], key=key(New_Jersey_Data_LONG_2015))
 New_Jersey_Data_LONG_2015[which(duplicated(New_Jersey_Data_LONG_2015))-1, VALID_CASE := "INVALID_CASE"]
 
 
-#  Still 5 kids with duplicates if Grade ignored -- take highest score and hope for the best ...
+#  Still 3 kids with duplicates if Grade ignored -- take highest score again ...
 setkey(New_Jersey_Data_LONG_2015, VALID_CASE, CONTENT_AREA, ID, SCALE_SCORE)
 setkey(New_Jersey_Data_LONG_2015, VALID_CASE, CONTENT_AREA, ID)
 New_Jersey_Data_LONG_2015[which(duplicated(New_Jersey_Data_LONG_2015))-1, VALID_CASE := "INVALID_CASE"]
-
 
 ### Subset new long data and save the results -- only keep variables already included in New_Jersey_SGP@Data
 
 load("Data/New_Jersey_SGP.Rdata")
 
-New_Jersey_Data_LONG_2015 <- New_Jersey_Data_LONG_2015[, c(intersect(names(New_Jersey_SGP@Data), names(New_Jersey_Data_LONG_2015)), "SCALE_SCORE_ACTUAL", "SCALE_SCORE_CSEM"), with=FALSE]
+setnames(New_Jersey_SGP@Data, gsub("[.]", "_", names(New_Jersey_SGP@Data)))
+
+New_Jersey_Data_LONG_2015 <- New_Jersey_Data_LONG_2015[, c(intersect(names(New_Jersey_SGP@Data), names(New_Jersey_Data_LONG_2015)), "esID", "SCALE_SCORE_ACTUAL", "SCALE_SCORE_CSEM"), with=FALSE]
 setkeyv(New_Jersey_Data_LONG_2015, c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID"))
 
 save(New_Jersey_Data_LONG_2015, file="Data/New_Jersey_Data_LONG_2015.Rdata")
+
+
+###
+###		Clean and prep 2014 New Jersey SGP object using SSID as ID
+###
+
+###  Set Data Names and @Names to new convention
+New_Jersey_SGP@Names <-  read.csv("/media/Data/Dropbox/Github_Repos/Packages/SGPstateData/variable_name_lookup/NJ_Variable_Name_Lookup.csv", colClasses=c(rep("character",4), "logical"))
+SGPstateData[["NJ"]][["Variable_Name_Lookup"]] <- read.csv("/media/Data/Dropbox/Github_Repos/Packages/SGPstateData/variable_name_lookup/NJ_Variable_Name_Lookup.csv", colClasses=c(rep("character",4), "logical"))
+
+setnames(New_Jersey_SGP@Data, gsub("[.]", "_", names(New_Jersey_SGP@Data)))
+
+###  Switch ID and 
+
+id.lookup <- unique(New_Jersey_Data_LONG_2015[, list(ID, esID, YEAR, VALID_CASE)])[!is.na(esID) & ID != "" & VALID_CASE=="VALID_CASE"]
+id.lookup <- id.lookup[, list(ID, esID)]
+setnames(id.lookup, c("ID", "esID"), c("SSID", "ID"))
+setkey(New_Jersey_SGP@Data, ID)
+setkey(id.lookup, ID)
+New_Jersey_SGP@Data <- id.lookup[New_Jersey_SGP@Data]
+
+table(is.na(New_Jersey_SGP@Data$Student_ID__SSID_), New_Jersey_SGP@Data$YEAR)
+New_Jersey_SGP@Data[which(is.na(Student_ID__SSID_)), Student_ID__SSID_ := SSID]
+New_Jersey_SGP@Data[, SSID := NULL]
+table(is.na(New_Jersey_SGP@Data$Student_ID__SSID_), New_Jersey_SGP@Data$YEAR)
+
+setnames(New_Jersey_SGP@Data, c("Student_ID__SSID_", "ID"), c("ID", "esID"))
+
+###  Invalidate NA SSIDs and duplicates
+New_Jersey_SGP@Data[which(is.na(ID)), VALID_CASE := "INVALID_CASE"]
+New_Jersey_SGP@Data[which(ID=="NULL"), VALID_CASE := "INVALID_CASE"]
+
+#  All duplicates (4 and 12 respectively below) are from 2013 and have different esID values.  Different kids?
+setkey(New_Jersey_SGP@Data, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID, SCALE_SCORE)
+setkey(New_Jersey_SGP@Data, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
+# sum(duplicated(New_Jersey_SGP@Data["VALID_CASE"]))
+# dups <- data.table(New_Jersey_SGP@Data[unique(c(which(duplicated(New_Jersey_SGP@Data))-1, which(duplicated(New_Jersey_SGP@Data)))), ], key=key(New_Jersey_SGP@Data))
+New_Jersey_SGP@Data[which(duplicated(New_Jersey_SGP@Data))-1, VALID_CASE := "INVALID_CASE"]
+
+setkey(New_Jersey_SGP@Data, VALID_CASE, CONTENT_AREA, YEAR, ID, SCALE_SCORE)
+setkey(New_Jersey_SGP@Data, VALID_CASE, CONTENT_AREA, YEAR, ID)
+New_Jersey_SGP@Data[which(duplicated(New_Jersey_SGP@Data))-1, VALID_CASE := "INVALID_CASE"]
+
+setkeyv(New_Jersey_SGP@Data, SGP:::getKey(New_Jersey_SGP))
+
+
+
+save(New_Jersey_SGP, file="Data/New_Jersey_SGP.Rdata")
--- New_Jersey_SGP_2015.R
+++ New_Jersey_SGP_2015.R
@@ -13,20 +13,21 @@ require(data.table)
 setwd('/media/Data/Dropbox/SGP/New_Jersey')
 load("Data/New_Jersey_Data_LONG_2015.Rdata")
 load("Data/New_Jersey_SGP.Rdata")
-
-### updateSGP
+setwd("/home/avi/NJ")
 
 ####  Custom Analysis Configurations
 ELA_2015.config <- list(
 	ELA.2015 = list(
-		sgp.content.areas=rep('ELA', 6),
+		sgp.content.areas=rep('ELA', 7),
 		sgp.panel.years=as.character(2009:2015),
 		sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('3', '4', '5', '6', '7'),
 								 c('3', '4', '5', '6', '7', '8'), c('3', '4', '5', '6', '7', '8', '9'))),
 	ELA.2015 = list( # Skip Year
-		sgp.content.areas=rep('ELA', 6),
-		sgp.panel.years=as.character(2008:2013, 2015),
+		sgp.content.areas=rep('ELA', 7),
+		sgp.panel.years=as.character(c(2008:2013, 2015)),
 		sgp.grade.sequences=list(c('3', '4', '5', '6', '7', '8', '10')),
+		sgp.exclude.sequences = data.table(VALID_CASE = 'VALID_CASE', CONTENT_AREA='ELA', 
+			YEAR='2014', GRADE='8'),
 		sgp.projection.grade.sequences="NO_PROJECTIONS"))
 
 MATHEMATICS_2015.config <- list(
@@ -35,40 +36,91 @@ MATHEMATICS_2015.config <- list(
 		sgp.panel.years=as.character(2010:2015),
 		sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('3', '4', '5', '6', '7'), c('3', '4', '5', '6', '7', '8'))),
  	ALGEBRA_I.2015 = list(
-		sgp.content.areas=c(rep('MATHEMATICS', 5), 'ALGEBRA_I'),
-		sgp.panel.years=as.character(2010:2015),
-		sgp.grade.sequences=list(c('3', '4', '5', '6', '7', 'EOCT')),
-		sgp.projection.grade.sequences="NO_PROJECTIONS"),
-	ALGEBRA_II.2015 = list(
-		sgp.content.areas=c(rep('MATHEMATICS', 6), 'ALGEBRA_II'),
-		sgp.panel.years=as.character(2008:2013, 2015),
+		sgp.content.areas=c(rep('MATHEMATICS', 6), 'ALGEBRA_I'),
+		sgp.panel.years=as.character(2009:2015),
+		sgp.grade.sequences=list(c('3', '4', '5', '6', '7', 'EOCT'), c('3', '4', '5', '6', '7', '8', 'EOCT')), # c('3', '4', '5', '6', 'EOCT'), # Sing D Mtx
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS")),
+	ALGEBRA_I.2015 = list( # Skip Year
+		sgp.content.areas=c(rep('MATHEMATICS', 6), 'ALGEBRA_I'),
+		sgp.panel.years=as.character(c(2008:2013, 2015)),
 		sgp.grade.sequences=list(c('3', '4', '5', '6', '7', '8', 'EOCT')),
+		sgp.exclude.sequences = data.table(VALID_CASE = 'VALID_CASE', CONTENT_AREA='MATHEMATICS', 
+			YEAR='2014', GRADE='8'),
+		sgp.projection.grade.sequences="NO_PROJECTIONS"),
+	# ALGEBRA_II.2015 = list( # Singular design matrix
+	# 	sgp.content.areas=c(rep('MATHEMATICS', 6), 'ALGEBRA_II'),
+	# 	sgp.panel.years=as.character(2009:2015),
+	# 	sgp.grade.sequences=list(c('3', '4', '5', '6', '7', '8', 'EOCT')),
+	# 	sgp.projection.grade.sequences="NO_PROJECTIONS"),
+	ALGEBRA_II.2015 = list( # Skip Year
+		sgp.content.areas=c(rep('MATHEMATICS', 6), 'ALGEBRA_II'),
+		sgp.panel.years=as.character(c(2008:2013, 2015)),
+		sgp.grade.sequences=list(c('3', '4', '5', '6', '7', '8', 'EOCT')),
+		sgp.exclude.sequences = data.table(VALID_CASE = 'VALID_CASE', CONTENT_AREA='MATHEMATICS', 
+			YEAR='2014', GRADE='8'),
+		sgp.projection.grade.sequences="NO_PROJECTIONS"),
+	# GEOMETRY.2015 = list( # Singular design matrix
+	# 	sgp.content.areas=c(rep('MATHEMATICS', 5), 'GEOMETRY'),
+	# 	sgp.panel.years=as.character(2010:2015),
+	# 	sgp.grade.sequences=list(c('3', '4', '5', '6', '7', 'EOCT')),
+	# 	sgp.projection.grade.sequences="NO_PROJECTIONS"),
+	GEOMETRY.2015 = list(
+		sgp.content.areas=c(rep('MATHEMATICS', 6), 'GEOMETRY'),
+		sgp.panel.years=as.character(2009:2015),
+		sgp.grade.sequences=list(c('3', '4', '5', '6', '7', '8', 'EOCT')),
+		sgp.projection.grade.sequences="NO_PROJECTIONS"),
+	GEOMETRY.2015 = list( # Skip Year
+		sgp.content.areas=c(rep('MATHEMATICS', 6), 'GEOMETRY'),
+		sgp.panel.years=as.character(c(2008:2013, 2015)),
+		sgp.grade.sequences=list(c('3', '4', '5', '6', '7', '8', 'EOCT')),
+		sgp.exclude.sequences = data.table(VALID_CASE = 'VALID_CASE', CONTENT_AREA='MATHEMATICS', 
+			YEAR='2014', GRADE='8'),
 		sgp.projection.grade.sequences="NO_PROJECTIONS"))
 
 NJ.config <- c(ELA_2015.config, MATHEMATICS_2015.config)
 
-SGPstateData[["NJ"]][["Assessment_Program_Information"]][["CSEM"]] <- "SCALE_SCORE_CSEM"
+
+### updateSGP
 
 New_Jersey_SGP <- updateSGP(
 			New_Jersey_SGP, 
 			New_Jersey_Data_LONG_2015,
 			sgp.config=NJ.config,
-			# steps = c("analyzeSGP", "combineSGP", "summarizeSGP", "visualizeSGP", "outputSGP"), #
+			# content.areas=c("ELA", "MATHEMATICS", "ALGEBRA_I", "ALGEBRA_I", "ALGEBRA_II", "GEOMETRY"),
+			steps = c("analyzeSGP", "combineSGP", "summarizeSGP", "outputSGP"), #
 			sgp.percentiles.baseline = FALSE,
 			sgp.projections.baseline = FALSE,
 			sgp.projections.lagged.baseline = FALSE,
-			save.intermediate.results=FALSE,
+			sgp.percentiles.equated=TRUE,
 			sgp.target.scale.scores=TRUE,			
-			goodness.of.fit.print=FALSE,
+			# outputSGP.output.type=c("LONG_Data", "LONG_FINAL_YEAR_Data"),
+			save.intermediate.results=FALSE,
 			parallel.config=list(
+				# BACKEND='FOREACH', TYPE="doParallel",
 				BACKEND="PARALLEL", 
 				WORKERS=list(
-					PERCENTILES=20, PROJECTIONS=20, LAGGED_PROJECTIONS=20, 
-					SGP_SCALE_SCORE_TARGETS=20, 
-					SUMMARY=20, 
-					GA_PLOTS=20, SG_PLOTS=1)))
+					PERCENTILES=10, PROJECTIONS=8, LAGGED_PROJECTIONS=8,
+					SGP_SCALE_SCORE_TARGETS=8,
+					SUMMARY=12)))
+
+###  Produce 2015 visualizations
+
+visualizeSGP(
+	New_Jersey_SGP,
+	# plot.types="studentGrowthPlot", #c("bubblePlot", 
+	sgPlot.content_areas = c("ELA", "MATHEMATICS"),
+	gaPlot.content_areas = c("ELA", "MATHEMATICS"),
+	sgPlot.demo.report=TRUE,
+	parallel.config=list(
+				BACKEND='FOREACH', TYPE="doParallel",
+				WORKERS=list(GA_PLOTS=12, SG_PLOTS=1)))
 
 
 ### Save results
 
+Summary <- New_Jersey_SGP@Summary
+New_Jersey_SGP@Summary <- NULL
+
 save(New_Jersey_SGP, file="Data/New_Jersey_SGP.Rdata")
+save(Summary, file="Data/New_Jersey_SGP_Summary_2015.Rdata")
+
